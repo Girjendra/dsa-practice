67b1062860f72177ed21ee3bf8112212c92306a7
Girjendra <shivaji12889@gmail.com>
2026-08-02 20:43:45 +0530

    smallest palindromic rearrangement II

---
+++ strings/02_smallest_palindromic_rearrangement_II.cpp
@@ -0,0 +1,77 @@
+/*
+You are given a palindromic string s and an integer k.
+
+Return the k-th lexicographically smallest palindromic permutation of s. If there are fewer than k distinct palindromic permutations, return an empty string.
+
+Note: Different rearrangements that yield the same palindromic string are considered identical and are counted once.
+*/
+#include<iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+using namespace std;
+
+// TC = O(nlogn + k * n), SC = O(n)
+class Solution {
+public:
+    bool solve(string &ans, int k) {
+        k--;
+
+        while (k--) {
+            if (!next_permutation(ans.begin(), ans.end()))
+                return false;
+        }
+
+        return true;
+    }
+
+    string smallestPalindrome(string s, int k) {
+        map<char, int> fre;
+
+        for(char ch : s)
+            fre[ch]++;
+
+        string ans;
+
+        char och = 'Z';
+        if(s.size() % 2) {
+            for(auto& it : fre) {
+                if(it.second % 2) {
+                    och = it.first;
+                    it.second--;
+                    break;
+                }
+            }
+        }
+
+        for(auto it : fre) {
+            int f = it.second;
+            char ch = it.first;
+            f /= 2;
+
+            while(f) {
+                ans.push_back(ch);
+                f--;
+            }
+        }
+
+        if (!solve(ans, k))
+            return ""; 
+
+        if(och != 'Z')
+            ans.push_back(och);
+
+        int i;
+        if(s.size() % 2)
+            i = ans.size() - 1;
+        else
+            i = ans.size();
+
+
+        while(i--) {
+            ans.push_back(ans[i]);
+        }
+
+        return ans;
+    }
+};
