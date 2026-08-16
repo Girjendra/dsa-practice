b86cb34dbc2be11fa401d3b1ec490f25257ae36d
Girjendra <shivaji12889@gmail.com>
2026-08-16 19:49:36 +0530

    Stone Game IX

---
+++ DP/41_stone_game_IX.cpp
@@ -0,0 +1,26 @@
+/*
+Alice and Bob continue their games with stones. There is a row of n stones, and each stone has an associated value. You are given an integer array stones, where stones[i] is the value of the ith stone.
+
+Alice and Bob take turns, with Alice starting first. On each turn, the player may remove any stone from stones. The player who removes a stone loses if the sum of the values of all removed stones is divisible by 3. Bob will win automatically if there are no remaining stones (even if it is Alice's turn).
+
+Assuming both players play optimally, return true if Alice wins and false if Bob wins.
+*/
+#include<iostream>
+#include <vector>
+using namespace std;
+
+// TC : O(n), SC: O(1)
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+
+        for(int x : stones)
+            cnt[x % 3]++;
+
+        if(min(cnt[1], cnt[2]) == 0)
+            return max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 == 1;
+
+        return abs(cnt[1] - cnt[2]) > 2 || cnt[0] % 2 == 0;
+    }
+};
